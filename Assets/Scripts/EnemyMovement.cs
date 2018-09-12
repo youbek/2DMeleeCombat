f1d7d8d24e05049cc23b13ecf79b02a5adffc5cd
youbek <youbekalimatov@gmail.com>
2018-09-13 01:07:40 +0500

    Initializing Git

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    #region PUBLIC_VAR
+    public int health = 100;
+    public bool hurted = false;
+    #endregion
+
+    #region PRIVATE_VAR
+    SpriteRenderer body;
+    Animator anim;
+    bool isDead = false;
+    int waitForDestroy = 1;
+    bool colorChanged = false;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hurted && isDead == false)
+        {
+            StartCoroutine(changeColor(new Vector4(1, 0.3f, 0.3f, 1)));
+            anim.SetTrigger("Hurt");
+            anim.SetBool("isIdle", false);
+            hurted = false;
+        } else if(hurted == false && isDead == false)
+        {
+            anim.ResetTrigger("Hurt");
+            anim.SetBool("isIdle", true);
+        }
+
+		if(health < 0)
+        {
+            isDead = true;
+            body.color = new Color(1, 1, 1, 1);
+            anim.SetTrigger("Hurt");
+            StartCoroutine(waitToBeDestory());
+            anim.SetBool("isIdle", false);
+        }
+
+        if (colorChanged)
+        {
+            body.color = new Color(1, 1, 1, 1);
+            colorChanged = false;
+        }
+
+	}
+
+    IEnumerator waitToBeDestory()
+    {
+        yield return new WaitForSeconds(waitForDestroy);
+        Destroy(gameObject);
+    }
+
+    IEnumerator changeColor(Vector4 color)
+    {
+        body.color = new Color(color.x, color.y, color.z, color.w);
+        yield return new WaitForSeconds(0.5f);
+        colorChanged = true;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,101 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    #region PUBLIC_VAR
+
+    #endregion
+    
+    #region PRIVATE_VAR
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private int damage;
+    Animator anim;
+    bool damageMade;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // if mouse left button clicked play attack animation
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isAttacking", true);
+        } else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+
+        // check if the attackPoint/damagemaker become acive
+        if(attackPoint.gameObject.active == true && damageMade == false)
+        {
+            damageMade = true;
+            Collider2D[] hittedObjects = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius);
+            if(hittedObjects.Length > 0)
+            {
+                for(int i = 0; i < hittedObjects.Length; i++)
+                {
+                    if(hittedObjects[i].gameObject != gameObject)
+                    {
+                        EnemyMovement enemy = hittedObjects[i].gameObject.GetComponent<EnemyMovement>();
+
+                        if (enemy != null)
+                        {
+                            enemy.health -= damage;
+                            enemy.hurted = true;
+                        }
+                    }
+                }
+            }
+        } else if(attackPoint.gameObject.active == false && damageMade == true)
+        {
+            damageMade = false;
+        }
+
+        // Check for axis horizontal
+        float movement = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+
+
+        // if movement is not equal to 0, means player pressed a or either d, so stop idling, else stop running
+        if (movement != 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("Attacking") == false)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isIdle", false);
+            // if movement float is more than 0 means that it moves to right, so turn player to right and move it
+            if (movement > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+            else if (movement < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+        } else if(movement == 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("Attacking") == false)
+        {
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isIdle", true);
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        if(attackPoint.gameObject.active == false)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+        } else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+        }
+    }
+}
